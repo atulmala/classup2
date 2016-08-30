2b58d6af871badc1762a11b991f32f06b490a007
Atul "the advanced prototype" Gupta <atulmala@gmail.com>
2016-08-30 22:03:41 +0530

    Customization of admin interface

--- teacher/admin.py
+++ teacher/admin.py
@@ -7,6 +7,6 @@ from .models import *
 
 class TeacherAdmin(admin.ModelAdmin):
     search_fields = ['first_name', 'last_name']
-    list_display = ['first_name', 'last_name', 'teacher_erp_id', 'school']
+    list_display = ['first_name', 'last_name', 'teacher_erp_id', 'email', 'school']
 
 admin.site.register(Teacher, TeacherAdmin)
