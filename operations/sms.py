d931e28a8306bcc3341ca4febc394be77a94d412
bharat <atul.gupta@classup.in>
2016-10-06 16:10:22 +0530

    SMS through bhash sms provider

--- operations/sms.py
+++ operations/sms.py
@@ -8,18 +8,19 @@ def send_sms(mobile, message):
     url1 += '&phone=' + mobile
     url1 += '&text=' + message
     url1 += '&priority=ndnd&stype=normal'
-    print(url1)
-    try:
-        response = urllib.urlopen(url1)
-        print(response)
-    except Exception as e:
-        print ('error occured while sending sms to ' + str(mobile))
-        print ('Exception = %s (%s)' % (e.message, type(e)))
-    print (response.read().decode('utf-8'))
+    #print(url1)
+    # try:
+    #     response = urllib.urlopen(url1)
+    #     print(response)
+    #     print (response.read().decode('utf-8'))
+    # except Exception as e:
+    #     print ('error occured while sending sms to ' + str(mobile))
+    #     print ('Exception = %s (%s)' % (e.message, type(e)))
 
 
 
 
+    print('sending to=' + mobile)
     url = "http://www.smscountry.com/smscwebservice_bulk.aspx"
     values = {
         'user': 'EmergeTech',
@@ -33,13 +34,38 @@ def send_sms(mobile, message):
     data = urllib.urlencode(values)
     data = data.encode('utf-8')
 
+    try:
+        response = urllib.urlopen(url, data)
+
+    except Exception as e:
+        print('error occured while sending sms to ' + str(mobile))
+        print('Exception = %s (%s)' % (e.message, type(e)))
+        print (response.read().decode('utf-8'))
+
 
 
 
 def send_sms_asynch(message_list):
     url = "http://www.smscountry.com/smscwebservice_bulk.aspx"
 
+    url1 = 'http://bhashsms.com/api/sendmsg.php?user=EMERGETECH&pass=kawasaki&sender=ClssUp'
+
+
+
     for number in message_list:
+
+        url1 += '&phone=' + str(number)
+        url1 += '&text=' + message_list[number]
+        url1 += '&priority=ndnd&stype=normal'
+        print(url1)
+        try:
+            response = urllib.urlopen(url1)
+            print(response)
+            print (response.read().decode('utf-8'))
+        except Exception as e:
+            print ('error occured while sending sms to ' + str(number))
+            print ('Exception = %s (%s)' % (e.message, type(e)))
+
         values = {
             'user': 'EmergeTech',
             'passwd': 'kawasaki#1',
@@ -58,4 +84,4 @@ def send_sms_asynch(message_list):
         except Exception as e:
             print('error occured while sending sms to ' + str(number))
             print('Exception = %s (%s)' % (e.message, type(e)))
-        print (response.read().decode('utf-8'))
+            print (response.read().decode('utf-8'))
