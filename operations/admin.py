977044e91954d034d84e9141e580e8cd33480e4e
Atul "the advanced prototype" Gupta <atulmala@gmail.com>
2017-02-01 23:57:23 +0530

    added fields to smsrecord model to retrieve the sms delivery status through batch program

--- operations/admin.py
+++ operations/admin.py
@@ -7,6 +7,6 @@ class SMSRecordAdmin(admin.ModelAdmin):
     list_display = ('school', 'sender1', 'sender_type', 'date',
                     'recipient_name', 'recipient_number', 'recipient_type', 'message', 'message_type', 'outcome',
                     'status_extracted', 'status',)
-    list_filter = ('school', 'date')
+    list_filter = ('school', 'date', 'status_extracted',)
 
 admin.site.register(SMSRecord, SMSRecordAdmin)
--- operations/get_sms_dlvry_status.py
+++ operations/get_sms_dlvry_status.py
@@ -1,21 +1,56 @@
 import MySQLdb
+import json
+import urllib
 
 print('Starting to extract sms delivery status')
 
 try:
+    # connect to the database
     db = MySQLdb.connect('classup-prod-1-aurora-cluster.cluster-ceglypsnyux3.us-west-2.rds.amazonaws.com',
                          'classup', 'classup', 'classup2')
-    cursor = db.cursor()
-    cursor.execute('SELECT VERSION()')
-    data = cursor.fetchone()
-    print('Database version is %s ' % data)
+    cursor1 = db.cursor()
 
-    sql = 'select outcome, status_extracted, status from operations_smsrecord where status_extracted='
-    cursor.execute(sql)
-    row =cursor.fetchone()
+    # extract message_id of all the sms sent after 31/01/17 for which sms delivery status has not been extracted
+    sql1 = "select outcome from operations_smsrecord where status_extracted=0 and date>'2017-01-31'"
+    cursor1.execute(sql1)
 
-    print(row)
+    # now, try to extract delivery status of each sms by calling api of the bulk sms provider
+    row = cursor1.fetchone()
+
+    message_id = row[0]
+    print(message_id)
+    status = 'Not Available'
+    try:
+        url = 'http://smppsmshub.in/api/mt/GetDelivery?user=atulg&password=atulg&jobid='
+        url += message_id
+        response2 = urllib.urlopen(url)
+        j2 = json.loads(response2.read())
+        print('j2=')
+        print(j2)
+        status = j2['DeliveryReports'][0]['DeliveryStatus'] + ' at '
+        status += j2['DeliveryReports'][0]['DeliveryDate']
+        print('status(smppsmshub)=' + str(status))
+    except Exception as e:
+        print('unable to get the staus of sms delivery. The url was: ')
+        print(url)
+        print ('Exception2 from operations get_sms_dlvry_status.py = %s (%s)' % (e.message, type(e)))
+
+    # update the smsrecord table that the delivery status of this sms has been extracted, hence no need
+    # to extract its delivery status when this program runs the next time
+    try:
+        cursor2 = db.cursor()
+        sql2 = "UPDATE operations_smsrecord SET status_extracted=1, status='" + status
+        sql2 += "' WHERE outcome=" + message_id
+        print(sql2)
+        cursor2.execute(sql2)
+        db.commit()
+        cursor2.close()
+    except Exception as e:
+        print('Error while trying to update the delivery status of sms with message_id = ' + message_id)
+        print ('Exception3 from operations get_sms_dlvry_status.py = %s (%s)' % (e.message, type(e)))
+
+    cursor1.close()
     db.close()
 except Exception as e:
     print('Error 1 from get_sms_delivery_status.py')
-    print('Exception3 from sms.py = %s (%s)' % (e.message, type(e)))
+    print('Exception1 from get_sms_dlvry_status.py = %s (%s)' % (e.message, type(e)))
