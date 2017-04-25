591770faad7b7a4b84a4475cd8275fcda141f9e6
Atul "the advanced prototype" Gupta <atulmala@gmail.com>
2017-04-25 16:28:24 +0530

    fixed urls.py of academics.

--- academics/urls.py
+++ academics/urls.py
@@ -34,7 +34,7 @@ urlpatterns = [
 
     url(r'get_attendance_summary/', views.get_attendance_summary, name='get_attendance_summary'),
 
-    url(r'delete_test/>\w+)/(?P<test_id>\w+)/$', views.delete_test, name='delete_test'),
+    url(r'delete_test/(?P<test_id>\w+)/$', views.delete_test, name='delete_test'),
 
     url(r'delete_hw/(?P<hw_id>\w+)$', views.delete_hw, name='delete_hw'),
 
