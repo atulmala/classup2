2328ca31191d59373adbeb0b38525c3980a05ddd
Atul "the advanced prototype" Gupta <atulmala@gmail.com>
2017-07-25 21:27:22 +0530

    Loggin user events

--- authentication/views.py
+++ authentication/views.py
@@ -48,6 +48,10 @@ class LogEntry(generics.ListCreateAPIView):
     def dispatch(self, request, *args, **kwargs):
         return super(LogEntry, self).dispatch(request, *args, **kwargs)
 
+    @method_decorator(csrf_exempt)
+    def post(self, request, *args, **kwargs):
+        pass
+
 
 # 23/07/2017 now we are implementing logging
 def log_entry(user, event, category, outcome):
