266863a68898483469b981d56dd466eefa94597b
Atul "the advanced prototype" Gupta <atulmala@gmail.com>
2017-08-11 17:43:38 +0530

    student list can be filtered by active status

--- classup2/urls.py
+++ classup2/urls.py
@@ -17,7 +17,7 @@ from django.conf.urls import include, url, patterns
 from django.contrib import admin
 
 urlpatterns = patterns(' ',
-
+    url(r'^grappelli/', include('grappelli.urls')), # grappelli URLS
     url(r'^', include('authentication.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^setup/', include('setup.urls')),
