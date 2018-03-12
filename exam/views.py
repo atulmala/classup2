4b1d464bebd603b9f6ed7f48c547068317e61383
Atul "the advanced prototype" Gupta <atulmala@gmail.com>
2018-03-12 20:23:25 +0530

    improved error handling for marksheet preparation

--- exam/views.py
+++ exam/views.py
@@ -860,7 +860,7 @@ def prepare_results(request, school_id, the_class, section):
 
                             except Exception as e:
                                 print('%s test for %s is not yet scheduled' % (term, sub))
-                                print('exception 12032018-A from exam views.py %s %s' % (e.message))
+                                print('exception 12032018-A from exam views.py %s %s' % (e.message, type(e)))
                         data1.append(sub_row)
                     except Exception as e:
                         print('Error while preparing results for %s in exam %s' % (sub, term))
