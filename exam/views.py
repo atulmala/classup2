6f6d3a48c9acc844ae8a496a748a059c27bae124
Atul "the advanced prototype" Gupta <atulmala@gmail.com>
2018-03-12 19:29:44 +0530

    improved error handling for marksheet preparation

--- exam/views.py
+++ exam/views.py
@@ -863,7 +863,7 @@ def prepare_results(request, school_id, the_class, section):
                                 print('exception 12032018-A from exam views.py %s %s' % (e.message))
                                 data1.append(sub_row)
                     except Exception as e:
-                        print('Error while preparing results')
+                        print('Error while preparing results for %s in exam %s' % (sub, term))
                         print ('Exception 21102017-A from exam views.py %s %s' % (e.message, type(e)))
                 try:
                     table1 = Table(data1)
