c6d7847c30cbbec52c9d951e845f8b06b838089d
Atul "the advanced prototype" Gupta <atulmala@gmail.com>
2018-03-20 22:40:35 +0530

    IX class marksheet not generating correctly

--- exam/views.py
+++ exam/views.py
@@ -560,8 +560,9 @@ def prepare_results(request, school_id, the_class, section):
                                 ('BOX', (0, 0), (-1, -1), 1, colors.black),
                                 ('TOPPADDING', (0, 0), (-1, -1), 1),
                                 ('BOTTOMPADDING', (0, 0), (-1, -1), 1),
+                                ('VALIGN', (0, 0), (-1, -1), 'TOP'),
+                                ('ALIGN', (0, 0), (1, 0), 'RIGHT'),
 
-                                ('SPAN', (0, 0), (1, 0)),
                                 ('FONTSIZE', (0, 0), (-1, -1), 8),
                                 ('FONT', (0, 0), (1, 0), 'Times-Bold')]
 
