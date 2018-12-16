f727237cbbfebc1e5d000a4dda7f8706274c51bf
Atul Gupta <atul.gupta@clasup.in>
2018-12-16 20:11:49 +0530

    User listed in order of date_joined and filtered by date_joined

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,7 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
 from .models import LoginRecord, LastPasswordReset, user_device_mapping, log_book
 
 # Register your models here.
+
+class MyUserAdmin(UserAdmin):
+    ordering = ('date_joined',)
+    list_display = ('username', 'date_joined', 'first_name', 'last_name',)
+    list_filter = ('date_joined',)
+
+admin.site.unregister(User)
+admin.site.register(User, MyUserAdmin)
 
 
 class LoginRecordAdmin(admin.ModelAdmin):
