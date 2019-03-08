fc978e11776c69cd45edc2a0bb1483849be2c598
Atul Gupta <atul.gupta@clasup.in>
2019-03-08 19:42:54 +0530

    new vendor api test

--- operations/sms_test.py
+++ operations/sms_test.py
@@ -1,40 +1,21 @@
 import urllib2
+import json
 
 mobile = '9873011186'
 message = 'Greetings+from+the+Chako'
-url = 'http://login.smsturtle.com/app/smsapi/index.php?key=25C7CB19C80D51&campaign=0&routeid=50&type=text'
-url += '&contacts=%s' % mobile
-url += '&senderid=DEMO'
-url += '&msg=%s' % message
+api_key = '6ZWRKLTUnEmMMQro3P30SQ'
+url = 'https://www.smsgatewayhub.com/api/mt/SendSMS?APIKey=%s' % api_key
+
+senderid = 'SMSTST'
+url += '&senderid=%s&channel=2&DCS=0&flashsms=0&number=%s&text=%s' % (senderid, mobile, message)
+
+
 print(url)
-for i in range(5):
+for i in range(1):
     response = urllib2.urlopen(url)
     print('response = ')
-    message_id = response.read()
-    print(message_id)
-    print(response.read())
-AWS_ACCESS_KEY_ID = 'AKIAJ6X32EHNR26CXSWA'
-AWS_SECRET_ACCESS_KEY = '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'
-
-# Create an SNS client
-# client = boto3.client(
-#     "sns",
-#     aws_access_key_id = AWS_ACCESS_KEY_ID,
-#     aws_secret_access_key = AWS_SECRET_ACCESS_KEY,
-#     region_name = "us-east-1"
-# )
-#
-# client.set_sms_attributes(
-#     attributes={
-#         'DefaultSMSType': 'Transactional',
-#         'DefaultSenderID': 'CLSSUP',
-#     }
-# )
-#
-# # Send your sms message.
-# result = client.publish(
-#     PhoneNumber = '+917678660426',
-#     Message = 'the schools will remain closed tomorrow on acccount of Diwal preparation'
-# )
-#
-# print(result)
+    outcome = json.loads(response.read())
+    print(outcome)
+    job_id = (outcome['JobId'])
+    print('job_id = ')
+    print(job_id)
