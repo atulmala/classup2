1d198370f4fc5f3a387be6997f0fcaf0b6df2561
Atul Gupta <atul.gupta@clasup.in>
2019-03-11 19:36:08 +0530

    Fixing issues in co-scholastc grades and result analysis sheet

--- exam/views.py
+++ exam/views.py
@@ -1388,7 +1388,8 @@ class ResultSheet(generics.ListCreateAPIView):
                 })
                 vertical_text.set_border()
                 perc_format = workbook.add_format({
-                    'num_format': '0.00%',
+                    'num_format': '0.0%',
+                    'font_size': 9,
                     'align': 'center',
                     'valign': 'vcenter',
                     'bold': True,
@@ -1681,12 +1682,13 @@ class ResultSheet(generics.ListCreateAPIView):
                                         gk_grade2 = ' '
                                         if index == 1:
                                             tr2 = TestResults.objects.get(class_test=gk_tests.last(), student=student)
-                                            gk_grade2 = '/%s' % tr2.grade
+                                            gk_grade2 = '%s' % tr2.grade
                                             print('GK grade secured by %s %s of %s-%s in %s: %s' %
                                                   (student.fist_name, student.last_name, the_class.standard,
                                                    section.section, term, gk_grade))
+                                            gk_grade = gk_grade2
+                                        #gk_grade = '%s%s' % (gk_grade1, gk_grade2)
 
-                                        gk_grade = '%s%s' % (gk_grade1, gk_grade2)
                                         print('GK grade secured by %s: %s' % (student_name, gk_grade))
                                     except Exception as e:
                                         print('exception 22032018-A from exam views.py %s %s' % (e.message, type(e)))
@@ -1901,21 +1903,26 @@ class ResultSheet(generics.ListCreateAPIView):
 
                         # co-scholastic grades. We will show grades for both terms separated by /, eg B/A
                         try:
-                            cs_term1 = CoScholastics.objects.get(term=term, student=student)
+                            cs_term1 = CoScholastics.objects.get(term='term1', student=student)
+                            cs_term2 = CoScholastics.objects.get(term='term2', student=student)
                             work_ed1 = cs_term1.work_education
-                            result_sheet.merge_range(row, col, row + 5, col,  work_ed1, cell_grade)
+                            work_ed2 = '%s/%s' % (work_ed1, cs_term2.work_education)
+                            result_sheet.merge_range(row, col, row + 5, col,  work_ed2, cell_grade)
                             col += 1
 
                             art_ed1 = cs_term1.art_education
-                            result_sheet.merge_range(row, col, row + 5, col, art_ed1, cell_grade)
+                            art_ed2 = '%s/%s' % (art_ed1,cs_term2.art_education)
+                            result_sheet.merge_range(row, col, row + 5, col, art_ed2, cell_grade)
                             col += 1
 
                             health_ed1 = cs_term1.health_education
-                            result_sheet.merge_range(row, col, row + 5, col, health_ed1, cell_grade)
+                            health_ed2 = '%s/%s' % (health_ed1, cs_term2.health_education)
+                            result_sheet.merge_range(row, col, row + 5, col, health_ed2, cell_grade)
                             col += 1
 
                             discipline1 = cs_term1.discipline
-                            result_sheet.merge_range(row, col, row + 5, col, discipline1, cell_grade)
+                            discipline2 = '%s/%s' % (discipline1, cs_term2.discipline)
+                            result_sheet.merge_range(row, col, row + 5, col, discipline2, cell_grade)
                             col += 1
 
                             teacher_remarks = cs_term1.teacher_remarks
@@ -1923,17 +1930,6 @@ class ResultSheet(generics.ListCreateAPIView):
                                   (student_name, the_class.standard, section.section, term, teacher_remarks))
                             result_sheet.merge_range(row, col, row + 5, col, teacher_remarks, cell_normal)
 
-                            cs_term2 = CoScholastics.objects.get(term='term2', student=student)
-                            work_ed2 = cs_term2.work_education
-                            result_sheet.write_string(row, 30, work_ed1 + '/' + work_ed2, cell_grade)
-                            art_ed2 = cs_term2.art_education
-                            result_sheet.write_string(row, 31, art_ed1 + '/' + art_ed2, cell_grade)
-                            health_ed2 = cs_term2.health_education
-                            result_sheet.write_string(row, 32, health_ed1 + '/' + health_ed2, cell_grade)
-                            discipline2 = cs_term2.discipline
-                            result_sheet.write_string(row, 33, discipline1 + '/' + discipline2, cell_grade)
-                            result_sheet.write_string(row, 34, gk_grade, cell_grade)
-                            # 11/10/2018 show teachers remarks
                             teacher_remarks = cs_term2.teacher_remarks
                             result_sheet.write_string(row, 35, teacher_remarks, cell_normal)
                         except Exception as e:
@@ -2237,10 +2233,10 @@ class ResultSheet(generics.ListCreateAPIView):
 
                                 # get the half yearly total cell
                                 cell = xl_rowcol_to_cell(row, col-5)
-                                if subject.subject_prac:
-                                    formula = '=%s * 25/%f' % (cell, subject.theory_marks)
-                                else:
-                                    formula = '=%s/4.0' % cell
+                                # if subject.subject_prac:
+                                formula = '=%s * 25/%f' % (cell, subject.theory_marks)
+                                # else:
+                                #     formula = '=%s/4.0' % cell
                                 result_sheet.write_formula(row, col, formula, cell_normal)
                                 col += 1
 
