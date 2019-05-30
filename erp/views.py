bb76883d62da426356787704a23ec44a7969b2c4
Atul Gupta <atul.gupta@clasup.in>
2019-05-30 20:51:08 +0530

    additional details remove decimals from student erp_id

--- erp/views.py
+++ erp/views.py
@@ -947,6 +947,11 @@ class SetupAddDetails(generics.ListCreateAPIView):
                     print ('Processing a new row')
                     try:
                         erp_id = sheet.cell(row, 1).value
+                        decimal = '.'
+                        if decimal in erp_id:
+                            print('student id contains a decimal followed by zero. This has to be removed')
+                            erp_id = erp_id[:-2]
+                            print('decimal and following zero removed. Now student_id = %s' % erp_id)
                         mother_name = (sheet.cell(row, 4).value).title()
                         address = sheet.cell(row, 5).value
                         house = (sheet.cell(row, 6).value).title()
