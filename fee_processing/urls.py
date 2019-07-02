55f33bf976dbbec1618e880faf9e8a0cddd73e27
Atul Gupta <atul.gupta@clasup.in>
2019-07-02 13:28:08 +0530

    fee_processing separate app some changes

--- fee_processing/urls.py
+++ fee_processing/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls import url, patterns
 
-from .views import SetupAddDetails, FeeDetails, ProcessFee, DefaulterReport, FeeHistory, CorrectFee, UploadFee
+from .views import FeeDetails, ProcessFee, DefaulterReport, FeeHistory, CorrectFee, UploadFee
 
 urlpatterns = patterns(
     '',
-    url(r'^setup_additional_details/$', SetupAddDetails.as_view(), name='setup_additional_details'),
+
     url(r'^fee_details/$', FeeDetails.as_view(), name='fee_payment',),
     url(r'^process_fee/(?P<school_id>\w+)/$', ProcessFee.as_view(), name='process_fee'),
     url(r'^defaulter_list/(?P<school_id>\w+)/$', DefaulterReport.as_view(), name='defaulter_list'),
