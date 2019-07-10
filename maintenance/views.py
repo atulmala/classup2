55955b9a76f23b8758b691e74bd56382433b65bc
Atul Gupta <atul.gupta@clasup.in>
2019-07-10 17:19:50 +0530

    Class Based view to retrieve SMS delivert status

--- maintenance/views.py
+++ maintenance/views.py
@@ -10,6 +10,7 @@ from datetime import datetime, timedelta
 
 from attendance.models import Attendance
 from operations.models import SMSRecord
+from teacher.models import MessageReceivers
 
 
 class JSONResponse(HttpResponse):
@@ -64,32 +65,68 @@ class SMSDeliveryStatus(generics.ListCreateAPIView):
 
         }
         time_threshold = datetime.now() - timedelta(hours=2.5)
-        records = SMSRecord.objects.filter(api_called=False, status_extracted=False,
+        print(time_threshold)
+        records = SMSRecord.objects.filter(api_called=True, status_extracted=False,
                                            date__lt=time_threshold)
         print('total %i messages delivery status to be extracted' % records.count())
         context_dict['message_count'] = records.count()
 
         for record in records:
+            print(record.date)
             delivery_id = record.outcome
+            print('delivery_id = %s' % delivery_id)
             print('extracting the delivery status of message with delivery id %s' % delivery_id)
-            if 'api' in delivery_id:
-                print('this message has been sent using SoftSMS API')
+            if delivery_id != '':
+                if 'api' in delivery_id:
+                    print('this message has been sent using SoftSMS API')
+                    try:
+                        url = 'http://softsms.in/app/miscapi/'
+                        key = '58fc1def26489'
+                        url += key
+                        url += '/getDLR/'
+                        url += delivery_id
+                        print('url=%s' % url)
+
+                        response = urllib.urlopen(url)
+                        status = response.read()
+                        print('status = ' + str(status))
+                    except Exception as e:
+                        print('unable to get the staus of sms delivery. The url was: ')
+                        print(url)
+                        print ('Exception 10072019-A from operations maintenance views.py = %s (%s)' % (e.message, type(e)))
+                else:
+                    print('message was sent using DealSMS API')
+                    url = 'http://5.9.69.238/reports/getByMid.php?uname=classup&password=56tr43we'
+                    the_date = record.date.strftime('%Y-%m-%d')
+                    url += '&sdate=%s' % the_date
+                    url += '&mid=%s' % delivery_id
+                    print('url = %s' % url)
+
+                    # now extract the delivery status
+                    try:
+                        response = urllib.urlopen(url)
+                        status = response.read()
+                        print('delivery status = %s' % status)
+                    except Exception as e:
+                        print('exception 10072019-B from maintenance views.py %s %s' % (e.message, type(e)))
+                        print('failed to extract he delivery status of message with id %s' % delivery_id)
                 try:
-                    url = 'http://softsms.in/app/miscapi/'
-                    key = '58fc1def26489'
-                    url += key
-                    url += '/getDLR/'
-                    url += delivery_id
-                    print('url=%s' % url)
-
-                    response = urllib.urlopen(url)
-                    status = response.read()
-                    print('status = ' + str(status))
+                    record.status = status
+                    record.status_extracted = True
+                    record.save()
+                    print('updated the delivery status of message with deliver_id %s' % delivery_id)
                 except Exception as e:
-                    print('unable to get the staus of sms delivery. The url was: ')
-                    print(url)
-                    print ('Exception2 from operations get_sms_dlvry_status.py = %s (%s)' % (e.message, type(e)))
+                    print('exception 10072019-C from maintenance views.py %s %s' % (e.message, type(e)))
+                    print('failed to save the extracted status of message with delivery id: %s' % delivery_id)
 
+                # update teacher message receiver record as well
+                try:
+                    receiver = MessageReceivers.objects.get(status=delivery_id)
+                    receiver.outcome = status
+                    receiver.status_extracted = True
+                    receiver.save()
+                    print('updated the delivery status of Teacher Message with delivery id %s' % delivery_id)
+                except Exception as e:
+                    print('exception 10072019-D from maintenance views.py %s %s' % (e.message, type(e)))
+                    print('failed to update Teacher Message Receiver with message id = %s' % delivery_id)
         return JSONResponse(context_dict, status=200)
-
-
