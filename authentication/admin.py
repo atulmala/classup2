43dabd172610b19ba2ef457d2914de97aa10dab0
Atul Gupta <atul.gupta@clasup.in>
2019-07-15 16:19:07 +0530

    Login records can be filtered by outcome (Success/Failed)

--- authentication/admin.py
+++ authentication/admin.py
@@ -5,6 +5,7 @@ from .models import LoginRecord, LastPasswordReset, user_device_mapping, log_boo
 
 # Register your models here.
 
+
 class MyUserAdmin(UserAdmin):
     ordering = ('-date_joined',)
     list_display = ('username', 'date_joined', 'first_name', 'last_name',)
@@ -20,12 +21,14 @@ class LoginRecordAdmin(admin.ModelAdmin):
     search_fields = ('login_id',)
     list_filter = ('date_and_time', 'outcome',)
 
+
 admin.site.register(LoginRecord, LoginRecordAdmin)
 
 
 class LastPasswordResetAdmin(admin.ModelAdmin):
     list_display = ('login_id', 'last_reset_time',)
     search_fields = ('login_id', 'last_reset_time',)
+
 
 admin.site.register(LastPasswordReset, LastPasswordResetAdmin)
 
@@ -34,6 +37,7 @@ class UserDeviceMappingAdmin(admin.ModelAdmin):
     list_display = ('user', 'mobile_number', 'token_id', 'device_type',)
     search_fields = ('mobile_number',)
 
+
 admin.site.register(user_device_mapping, UserDeviceMappingAdmin)
 
 
@@ -41,5 +45,6 @@ class LogBookAdmin(admin.ModelAdmin):
     list_display = ('date_and_time', 'user', 'school', 'user_name', 'event', 'category', 'outcome',)
     search_fields = ('user', 'user_name',)
     list_filter = ('date_and_time',)
+
 
 admin.site.register(log_book, LogBookAdmin)
