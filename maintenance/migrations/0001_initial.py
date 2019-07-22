b2741327dd04cee8cee6a69b1f2822201cd69077
Atul Gupta <atul.gupta@clasup.in>
2019-07-22 13:36:49 +0530

    sms delivery stats now stored into database for later analysis

---
+++ maintenance/migrations/0001_initial.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='SMSDelStats',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('start_time', models.DateTimeField()),
+                ('end_time', models.DateTimeField()),
+                ('time_taken', models.DecimalField(max_digits=16, decimal_places=8)),
+                ('messages_count', models.IntegerField()),
+            ],
+        ),
+    ]
