69aafd466e7a7bcb3cbcf0eca98c8679909bc0e9
Atul Gupta <atul.gupta@classup.in>
2020-01-24 23:09:29 +0530

    settings for django-debug-toolbar

--- classup2/urls.py
+++ classup2/urls.py
@@ -13,15 +13,16 @@ Including another URLconf
     1. Add an import:  from blog import urls as blog_urls
     2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
+
 from django.conf.urls import include, url, patterns
 from django.contrib import admin
+from classup2 import settings
 
 
 urlpatterns = patterns(' ',
     url(r'^grappelli/', include('grappelli.urls')), # grappelli URLS
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', include('authentication.urls')),
-
     url(r'^setup/', include('setup.urls')),
     url(r'^academics/', include('academics.urls')),
     url(r'^student/', include('student.urls')),
@@ -40,6 +41,12 @@ urlpatterns = patterns(' ',
     url(r'analytics/', include('analytics.urls')),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 )
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += (
+        url(r'^__debug__/', include(debug_toolbar.urls)),
+    )
 
 
 
