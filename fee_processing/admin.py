a3b1a3e65acc9173e9ed59f17526b6316349ce76
Atul Gupta <atul.gupta@classup.in>
2020-05-02 19:40:02 +0530

    fee defaulters

--- fee_processing/admin.py
+++ fee_processing/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from fee_processing.models import FeeDefaulters
+
+
+@admin.register(FeeDefaulters)
+class FeeDefaultersAdmin(admin.ModelAdmin):
+    list_display = ('student', 'amount_due',)
